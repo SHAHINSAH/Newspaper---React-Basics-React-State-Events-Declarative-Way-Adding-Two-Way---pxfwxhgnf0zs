f16af107479a78fb63974955e38e0f2ed07e4d5f
Shahinsah Kumar <shahinsahkumar.com>
2023-01-13 02:56:09 +0530

    change2

--- src/components/App.js
+++ src/components/App.js
@@ -14,8 +14,8 @@ const App = () => {
     {
       fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${'4a4f1ce1953c34c9ba2088f874784df5'}`, { method: "GET" }).then(e => e.json()).then(e => {
         setWeather(JSON.stringify({ temperature: e.main.temp, location: e.coord }))
-      })
-      fetch(`https://gnews.io/api/v4/search?q=example&token=410db42779f25b2d81028050efe65502&max=5`).then(e => e.json()).then(e => {
+      })                                                    
+      fetch(`https://gnews.io/api/v4/search?q=example&token=a4bae192d02391945551737e4af056f5&max=5`).then(e => e.json()).then(e => {
         // for(int )  
         setNews(JSON.stringify({ key: "1", title: e.articles[0].title, description: e.articles[0].description }))
       })
