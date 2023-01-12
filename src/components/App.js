8f5029d3efd231318383792a536a4c2a1b4103e9
Shahinsah Kumar <shahinsahkumar.com>
2023-01-13 02:43:37 +0530

    change2

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,11 @@ const App = () => {
   const [long, setlongi] = useState("")
   const [newlocation, setnewlocatio] = useState("")
 
-  useEffect(() => {
+  useEffect(() => {                                    
     if (lat !== "" && long !== "")
     // also runs if one of lat or long is updated
     {
-      fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${'da05b6e0fe788db7dcdc421c300214c1'}`, { method: "GET" }).then(e => e.json()).then(e => {
+      fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${'4a4f1ce1953c34c9ba2088f874784df5'}`, { method: "GET" }).then(e => e.json()).then(e => {
         setWeather(JSON.stringify({ temperature: e.main.temp, location: e.coord }))
       })
       fetch(`https://gnews.io/api/v4/search?q=example&token=410db42779f25b2d81028050efe65502&max=5`).then(e => e.json()).then(e => {
@@ -24,7 +24,7 @@ const App = () => {
   }, [lat, long])
 
   const findFromCity = (e) => {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${e}&appid=${'da05b6e0fe788db7dcdc421c300214c1'}`, { method: "GET" }).then(e => e.json()).then(e => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${e}&appid=${'4a4f1ce1953c34c9ba2088f874784df5'}`, { method: "GET" }).then(e => e.json()).then(e => {
       setWeather(JSON.stringify({ key: "2", temperature: e.main.temp, location: e.coord }))
     }).catch(e => {
       setWeather(JSON.stringify({ key: "3", message: "invalid location" }))
